02af31d98d217df1810370575d1e32afe7d7e97d
Farrukh <muhammadfarrukh2001@gmail.com>
2021-12-05 00:51:12 +0500

    Fixed Search & Add Courses

--- project/search.py
+++ project/search.py
@@ -4,6 +4,7 @@ from flask.templating import render_template_string
 from werkzeug.utils import redirect
 from project import views, auth, add
 from .db_config import create_db
+from project.auth import session
 
 mysql = create_db()
 
@@ -27,6 +28,7 @@ def index():
         print(type(result)) 
         if result[0] != None:
             #return render_template('output.html', headings = ("CourseID","Title","DepartmentName","Credits","ModesOfInstruction") ,variable = result)
+            session["search"] = result
             return redirect(url_for('search.output', data = result))
 
     return render_template('search_course.html')
@@ -34,5 +36,7 @@ def index():
 @search.route('/search_output/<data>', methods=['GET', 'POST'])
 def output(data):
     data = list(eval(data)) 
-    print("check:", data    )   
-    return render_template('search_output.html', headings = ("CourseID","Title","DepartmentName","Credits","ModesOfInstruction") , variable = data)
+    print("check:", data    )
+    r = session["search"]
+    print("Value of r:", r)   
+    return render_template('search_output.html', headings = ("CourseID","Title","DepartmentName","Credits","ModesOfInstruction") , variable = r)
