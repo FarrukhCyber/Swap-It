b9d110162baf18ba9427ae1be793ad6cdfd223b7
Farrukh <muhammadfarrukh2001@gmail.com>
2021-12-14 22:12:02 +0500

    remote db configs

--- project/db_config.py
+++ project/db_config.py
@@ -3,11 +3,11 @@ from flask_mysqldb import MySQL
 
 # MYSQL DB configuartions
 def create_db():
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_HOST'] = 'us-cdbr-east-05.cleardb.net'
+    app.config['MYSQL_USER'] = 'bb29c37c5803df'
 
-    app.config['MYSQL_PASSWORD'] = '786@mf12'
-    app.config['MYSQL_DB'] = 't3'
+    app.config['MYSQL_PASSWORD'] = '9fc3e4c6'
+    app.config['MYSQL_DB'] = 'heroku_58cd6fc93d90184'
     mysql = MySQL(app)
     
     return mysql
